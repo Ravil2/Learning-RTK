b4e5641d622b235bfbd734047416ff29a11ac04c
Ravil2 <rafffa141105@gmail.com>
2025-08-14 11:06:17 +0500

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import {
-  decrement,
-  decrementByAmount,
-  increment,
-  incrementByAmount,
-} from './store/counterSlice'
-import UserCounter from './components/UserCounter'
 import { useState } from 'react'
 
 export default function App() {
-  const count = useSelector((state) => state.counter.count)
-  const dispatch = useDispatch()
-  const [amount, setAmount] = useState(0)
+  const [text, setText] = useState('')
+  const [todos, setTodos] = useState([])
 
-  const plusOne = () => {
-    dispatch(increment())
+  const handleInput = (task) => {
+    if (task) {
+      setText(task)
+    }
   }
-
-  const minusOne = () => {
-    dispatch(decrement())
-  }
-
-  const handleChange = (e) => {
-    setAmount(e.target.value)
-  }
-
-  const plusMany = () => {
-    dispatch(incrementByAmount(Number(amount)))
-  }
-
-  const minusMany = () => {
-    dispatch(decrementByAmount(Number(amount)))
+  const addTodo = () => {
+    if (text.trim()) {
+      setTodos([...todos, { id: Date.now(), text: text, completed: false }])
+    }
+    setText('')
   }
 
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={plusOne}>+</button>
-      <button onClick={minusOne}>-</button>
-      <UserCounter />
-      <p>----------------------------------------------</p>
-      <input type="number" value={amount} onChange={handleChange} />
-      <button onClick={plusMany}>+</button>
-      <button onClick={minusMany}>-</button>
-    </div>
+    <>
+      <div className="flex flex-col p-4">
+        <label className="flex flex-col gap-4 w-full items-center bg-white-900  rounded-2xl p-6 shadow-lg">
+          <input
+            value={text}
+            type="text"
+            onChange={(e) => handleInput(e.target.value)}
+            placeholder="Добавить задачу..."
+            className="w-full p-3 rounded-lg border border-black focus:border-black focus:ring-2 focus:ring-blue-400 outline-none transition-all duration-200 placeholder:text-black-600/70"
+          />
+          <button
+            onClick={addTodo}
+            className="px-6 py-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-700 active:scale-95 transition-transform duration-150"
+          >
+            Добавить задачу
+          </button>
+        </label>
+        <ul className="mt-5">
+          {todos.map((todo) => (
+            <li
+              className="flex justify-between border-2 px-4 py-2 rounded-sm"
+              key={todo.id}
+            >
+              {todo.text}{' '}
+              <button>completed: {todo.completed ? 'true' : 'false'}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
--- src/components/UserCounter.jsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux'
-
-export default function UserCounter() {
-  const count = useSelector((state) => state.counter.count)
-
-  return <div>{count}</div>
-}
